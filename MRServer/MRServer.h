61b5b88acad621a48273f53e2f7e0c249d1b7a51
Steven <svaughan@purdue.edu>
2018-10-19 23:49:29 -0400

    Restructured repo

---
+++ MRServer/MRServer.h
@@ -0,0 +1,19 @@
+
+#ifndef MR_SERVER
+#define MR_SERVER
+
+
+class MRServer {
+	// Add any variables you need
+
+private:
+	int open_server_socket(int port);
+
+public:
+		
+
+	void initialize();
+	void runServer(int port);
+};
+
+#endif
